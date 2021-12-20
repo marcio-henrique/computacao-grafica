8603be83e78a23911cd56dc36a00e8bc54f5db97
Márcio Henrique <marcio.henrique@doity.com.br>
2021-12-20 07:57:59 -0300

    Sistema solar

---
+++ projetos/sistema-solar/README.md
@@ -0,0 +1,21 @@
+# Sistema Solar
+
+![Sistema Solar](https://github.com/marcio-henrique/computacao-grafica/blob/main/imgs/sistema-solar.png)
+
+## Especificações:
+
+Criar um sistema solar com 5 planetas em OpenGL que apresente o movimento de Translação de 5 Planetas ao redor do Sol (os planetas devem apresentar orbitas distintas, velocidades variadas) quando o usuário clicar na tecla Y.
+
+
+
+- Um dos planetas deve possuir duas luas que
+  realizam o movimento de translação ao redor do planeta, uma lua gira no eixo x e a outra no eixo xy;
+  
+## Considerações
+
+- Teclas:
+  - 'y': translação da direita para a esquerda
+  - 'Y': translação da esquerda para a direita
+ 
+- Link do vídeo com a execução do programa: [aqui](https://www.loom.com/share/4cb13560c9df4293a7ee98aa7a719348)
+
---
+++ projetos/sistema-solar/main.cpp
@@ -0,0 +1,131 @@
+
+#include <GL/gl.h>
+#include <GL/glut.h>
+#include <stdlib.h>
+
+#define AZUL     0.0, 0.0, 1.0
+#define VERMELHO 1.0, 0.0, 0.0
+#define AMARELO  1.0, 1.0, 0.0
+#define VERDE    0.0, 1.0, 0.0
+#define CYAN     1.0, 0.0, 1.0
+#define LARANJA  0.8, 0.6, 0.1
+#define ROSEO    0.7, 0.1, 0.6
+#define CINZA    0.6, 0.6, 0.6
+
+int year = 0;
+
+void init(void){
+    glClearColor (0.0, 0.0, 0.0, 0.0);
+}
+
+void displaySpheres (void) {
+    glPushMatrix();
+    glColor3f(AMARELO);
+    glutWireSphere(0.6, 16, 16);   /* desenha o sol */
+    glPopMatrix();
+
+    glPushMatrix();
+    glColor3f(VERMELHO);
+    glRotatef ( 1.5 * year, 0.0, 1.0, 0.0);
+    glTranslatef (2.45, 0.1, 2.0); //Translada a partir do novo sistema de coordenadas resultante da Rotacao
+    glutWireSphere(0.09, 16, 16);    /* desenha um planeta */
+    glPopMatrix();
+
+    glPushMatrix();
+    glColor3f(CINZA);
+    glRotatef ((GLfloat) year, 0.0, 1.2, 0.0);
+    glTranslatef(2.0, 0, 0);
+    glPushMatrix();
+    glutWireSphere(0.2, 16, 16); // Generate moon
+    glPopMatrix();
+
+    glPushMatrix();
+    glColor3f(LARANJA);
+    glRotatef (1.7 * year, 0.0, 1.1, 0.0);
+    glTranslatef(0.3, 0, 0);
+    glutWireSphere(0.05, 16, 16); // Generate moon
+    glPopMatrix();
+
+    glPushMatrix();
+    glColor3f(CYAN);
+    glRotatef (2.1 * year, 1.1, 1.1, 0.0);
+    glTranslatef(0.3, 0, 0);
+    glutWireSphere(0.03, 16, 16); // Generate moon
+    glPopMatrix();
+    glPopMatrix();
+
+    glPushMatrix();
+    glColor3f(AZUL);
+    glRotatef (0.2 * year, 0.0, 1.3, 0.0);
+    glTranslatef(3.45, 0, 0);
+    glutWireSphere(0.3, 16, 16); // Generate moon
+    glPopMatrix();
+
+    glPushMatrix();
+    glColor3f(VERDE);
+    glRotatef (2 * year, 0.0, 1.4, 0.0);
+    glTranslatef(1.2, 0, 0);
+    glutWireSphere(0.1, 16, 16); // Generate moon
+    glPopMatrix();
+
+    glPushMatrix();
+    glColor3f(ROSEO);
+    glRotatef (3 * year, 0.0, 1.5, 0.0);
+    glTranslatef(3.1, 0, 0);
+    glutWireSphere(0.24, 16, 16); // Generate moon
+    glPopMatrix();
+
+
+}
+
+void display(void){
+    glClear (GL_COLOR_BUFFER_BIT);
+
+    displaySpheres();
+
+    glutSwapBuffers(); //pois estamos adotando o GLUT_DOUBLE
+}
+
+void reshape (int w, int h){
+    glViewport (0, 0, (GLsizei) w, (GLsizei) h);
+
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+
+    glMatrixMode(GL_PROJECTION);
+    gluPerspective(60.0, (GLfloat) w/(GLfloat) h, 1.0, 20.0);
+    gluLookAt (0.0, 0.0, 5.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0); //posicao da camera
+}
+
+void keyboard (unsigned char key, int x, int y){
+    switch (key) {
+        case 'y':
+            year = (year + 5);
+            glutPostRedisplay();
+            break;
+        case 'Y':
+            year = (year - 5);
+            glutPostRedisplay();
+            break;
+        case 27:
+            exit(0);
+            break;
+        default:
+            break;
+    }
+}
+
+int main(int argc, char** argv){
+    glutInit(&argc, argv);
+    glutInitDisplayMode (GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+    glutInitWindowSize (800, 600);
+    glutInitWindowPosition (100, 100);
+    glutCreateWindow ("Sistema Solar");
+
+    init ();
+    glutDisplayFunc(display);
+    glutKeyboardFunc(keyboard);
+    glutReshapeFunc(reshape);
+    glutMainLoop();
+    return 0;
+}
